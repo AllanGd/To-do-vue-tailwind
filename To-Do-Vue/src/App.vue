8ca4f83a7a08032fec6d7697543320fae92153b1
AllanGd <allan_gd@hotmail.com>
2023-04-13 22:51:00 -0300

    dimension adjustment

--- To-Do-Vue/src/App.vue
+++ To-Do-Vue/src/App.vue
@@ -3,17 +3,16 @@ import Tarefa from '../src/components/Tarefa.vue'
 </script>
 
 <template>
-  <div class="flex items-center justify-center pt-4">
+  <div id="task-form" class="mb-4 flex items-center justify-center pt-4">
     <input type="text" value="" placeholder="Digite sua tarefa aqui"
-      class="h-11 w-2/4 rounded-l-md border-2 border-emerald-200 bg-emerald-50 p-2 font-medium text-slate-600 placeholder-slate-400 focus:bg-green-100 focus:outline-none" />
+      class="h-11 w-7/12 rounded-l-md border-2 border-emerald-200 bg-emerald-50 p-2 font-medium text-slate-600 placeholder-slate-400 focus:bg-green-100 focus:outline-none" />
     <input type="date"
-      class="h-11 border-2 border-emerald-200 bg-emerald-50 p-2 text-slate-600 hover:border-emerald-300 hover:bg-emerald-200 focus:border-emerald-600 focus:bg-emerald-300"
+      class="h-11 border-2 w-1/12 border-emerald-200 bg-emerald-50 p-2 text-slate-600 hover:border-emerald-300 hover:bg-emerald-200 focus:border-emerald-600 focus:bg-emerald-300"
       name="" id="" />
     <input type="button"
-      class="h-11 rounded-r-md border-2 border-emerald-200 bg-emerald-200 p-2 font-semibold text-green-700 hover:bg-emerald-300 focus:border-emerald-600 focus:bg-emerald-300"
+      class="h-11 rounded-r-md w-2/12 border-2 border-emerald-200 bg-emerald-200 p-2 font-semibold text-green-700 hover:bg-emerald-300 focus:border-emerald-600 focus:bg-emerald-300"
       value="Adicionar" />
   </div>
-  <Tarefa></Tarefa>
-  </template>
+  <Tarefa></Tarefa></template>
 
 <style scoped></style>
