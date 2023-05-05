be1d85c004af1fc21812ddba3d6af6cb94157e68
AllanGd <allan_gd@hotmail.com>
2023-05-05 00:36:51 -0300

    interface update

--- To-Do-Vue/src/App.vue
+++ To-Do-Vue/src/App.vue
@@ -1,18 +1,32 @@
-<script setup lang="ts">
-import Tarefa from '../src/components/Tarefa.vue'
-</script>
-
 <template>
-  <div id="task-form" class="mb-4 flex items-center justify-center pt-4">
-    <input type="text" value="" placeholder="Digite sua tarefa aqui"
-      class="h-11 w-7/12 rounded-l-md border-2 border-emerald-200 bg-emerald-50 p-2 font-medium text-slate-600 placeholder-slate-400 focus:bg-green-100 focus:outline-none" />
-    <input type="date"
-      class="h-11 border-2 w-1/12 border-emerald-200 bg-emerald-50 p-2 text-slate-600 hover:border-emerald-300 hover:bg-emerald-200 focus:border-emerald-600 focus:bg-emerald-300"
-      name="" id="" />
-    <input type="button"
-      class="h-11 rounded-r-md w-2/12 border-2 border-emerald-200 bg-emerald-200 p-2 font-semibold text-green-700 hover:bg-emerald-300 focus:border-emerald-600 focus:bg-emerald-300"
-      value="Adicionar" />
+  <div class="bg-gray-200 pb-5 drop-shadow-lg">
+    <div class="container mx-auto p-1">
+      <div class="mt-4 grid gap-3 md:grid-cols-3 lg:grid-cols-6">
+        <input type="text" name=""
+          class="md:col-span-3 border-2 border-gray-300 bg-gray-100 p-1 text-lg text-gray-500 outline-none max-lg:rounded-t-md lg:rounded-l-md"
+          placeholder="Digite aqui" id="" />
+        <input type="date" name=""
+          class="col-span-1 border-2 border-gray-300 bg-gray-100 p-1 text-lg text-gray-500 outline-none md:rounded-bl-md lg:rounded-none"
+          id="" />
+        <input type="button"
+          class="col-span-1 border-2 border-gray-300 bg-gray-300 p-1 text-lg font-bold hover:bg-gray-400 active:bg-gray-500 max-md:rounded-b-md md:col-span-2 md:rounded-br-md lg:rounded-r-md"
+          value="Adicionar" @click="teste" />
+      </div>
+    </div>
   </div>
-  <Tarefa></Tarefa></template>
+  <div class="container mx-auto p-1 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-3 gap-y-5 pt-5">
+    <div v-for="index in count"><tarefa/></div>
+  </div>
+</template>
 
 <style scoped></style>
+<script setup lang="ts">
+import { ref } from 'vue';
+import tarefa from './components/Tarefa.vue'
+
+let count =ref(0);
+
+function teste() {
+  count.value++;
+}
+</script>
--- To-Do-Vue/src/components/Tarefa.vue
+++ To-Do-Vue/src/components/Tarefa.vue
@@ -1,34 +1,32 @@
 <template>
-  <div id="task-card" class="flex w-10/12 items-center justify-center">
-  <div id="card" class="w-3/4 rounded-md border-2 border-b-8 border-emerald-200 border-b-emerald-500 bg-emerald-50 p-4">
-    <div id="card-title" class="mb-4 flex justify-between">
-      <span class="text-lg font-semibold text-slate-600">Tarefa</span>
-      <div id="date">
-        <div>
-          <div class="flex items-center text-slate-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <span class="ml-2">10/03/2023</span>
-          </div>
-          <div class="text-right">Atrasado</div>
-        </div>
-      </div>
+  <div class="grid-col-3 grid gap-1">
+    <div class="col-span-3 grid grid-cols-2 rounded-t-md border-2 border-gray-300 bg-gray-100 p-2 text-lg text-gray-500">
+      <span class="col-span-2 row-span-2 mt-2 h-24">teste card de tarefa</span>
+      <span class="self-end text-base">Prazo: 15/03/2023</span>
+      <span class="self-end text-base font-bold text-orange-600 justify-self-end">Próximo do vencimento</span>
     </div>
-    <div id="task-description" class="mb-4 text-slate-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis praesentium obcaecati accusantium vero fuga sit provident vel fugiat dolor illum. Libero, eaque sapiente minima enim officia aliquam nesciunt possimus repudiandae.</div>
-    <div class="flex justify-between">
-      <div class="flex items-center gap-2 rounded-md border-2 border-emerald-200 bg-emerald-100 px-2 hover:border-emerald-400 hover:bg-emerald-200">
-        <input type="checkbox" name="Concluido" id="Concluido" class="h-4 w-4 rounded border-green-300 bg-emerald-200 text-emerald-600" />
-        <span class="font-semibold text-slate-600">Concluido 08/03/2023</span>
-      </div>
-      <div id="buttons" class="flex gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7 rounded-md border-2 border-emerald-200 bg-emerald-100 text-blue-500 hover:border-emerald-400 hover:bg-emerald-200">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7 rounded-md border-2 border-emerald-200 bg-emerald-100 text-red-500 hover:border-emerald-400 hover:bg-emerald-200">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-        </svg>
-      </div>
-    </div>
+    <button class="col-span-1 rounded-bl-md drop-shadow-md bg-emerald-400 p-1 text-lg font-bold hover:bg-emerald-500 active:bg-emerald-600">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline h-6 w-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+      Concluir
+    </button>
+    <button class="col-span-1 bg-sky-400 p-1 drop-shadow-md text-lg font-bold hover:bg-sky-500 active:bg-sky-600">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline h-6 w-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+      </svg>
+      Editar
+    </button>
+    <button class="col-span-1 rounded-br-md bg-red-400 drop-shadow-md p-1 text-lg font-bold hover:bg-red-500 active:bg-red-600">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline h-6 w-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+      </svg>
+      Remover
+    </button>
   </div>
-</div></template>
+</template>
+<script lang="ts">
+  export default {
+    
+  }
+</script>
